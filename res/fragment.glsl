6809139e4b11d2cc6827bd1261f4c36ea7b632e4
Aznes <timotheefageot@outlook.fr>
2025-03-13 16:47:25 +0100

    lighted boat

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -4,11 +4,12 @@ out vec4 out_color;
 in vec3 vertex_position;
 uniform sampler2D my_texture;
 in vec2 uv;
+in vec3 normal;
+//const vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));
+uniform vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));;
 
 void main()
 {
-    //out_color = vec4(vertex_position, 1.);
-
     vec4 texture_color = texture(my_texture, uv);
-    out_color = texture_color;
+    out_color = texture_color * dot(normalize(normal), light_direction);
 }
--- res/vertex.glsl
+++ res/vertex.glsl
@@ -2,6 +2,7 @@
 
 layout(location = 0) in vec3 in_position;
 layout(location = 1) in vec2 in_uv;
+layout(location = 2) in vec3 in_normal;
 
 
 uniform float aspect_ratio;
@@ -10,16 +11,16 @@ uniform mat4 view_projection_matrix; // On ajoute une uniforme pour la matrice d
 
 out vec3 vertex_position;
 out vec2 uv;
+out vec3 normal;
 
 void main()
 {
     vec3 position = in_position;
-    // ...
+
     position.x += offset;
-    //position.x /= aspect_ratio;
-    // ...
-    //gl_Position = vec4(position, 0.4f, 1.0f); // Ici on utilise maintenant `position` et non plus `in_position`
+
     gl_Position = view_projection_matrix * vec4(in_position, 1.);
     vertex_position = in_position;
     uv = in_uv;
+    normal = in_normal;
 }
