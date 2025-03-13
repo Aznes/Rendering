7ea0f2d8d48f1164b8519d92cb49709792ff830c
Aznes <timotheefageot@outlook.fr>
2025-03-13 17:32:51 +0100

    point light

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -6,10 +6,14 @@ uniform sampler2D my_texture;
 in vec2 uv;
 in vec3 normal;
 //const vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));
-uniform vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));;
+uniform vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));
+uniform vec3 light_position = vec3(1., 0., 0.);
 
 void main()
 {
     vec4 texture_color = texture(my_texture, uv);
-    out_color = texture_color * (clamp(dot(normalize(normal), light_direction),0 ,1) + 0.3);
+    float squared_light_distance = distance(vertex_position, light_position) * distance(vertex_position, light_position);
+    vec3 light_direction = normalize(vertex_position-light_position);
+    vec4 lighted_color = texture_color * (clamp(dot(normalize(normal), light_direction) / squared_light_distance,0 ,1) + 0.3);
+    out_color = lighted_color;
 }
